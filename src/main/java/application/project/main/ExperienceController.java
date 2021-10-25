64fca276f4794255aa1081c46e5502c2345d595a
Ari 2.0 <afainerman@correo.um.edu.uy>
2021-10-25 20:58:26 -0300

    Cambios

--- src/main/java/application/project/main/ExperienceController.java
+++ src/main/java/application/project/main/ExperienceController.java
@@ -4,4 +4,24 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ExperienceController {
+
+    ExperienciaEntity experiencia;
+
+    @FXML
+    private Text txtExperienceName;
+
+    @FXML
+    private Text txtExperienceDescription;
+
+    @FXML
+    private Button btnImgSiguiente;
+
+    public void initialize(){
+        Stage stage = (Stage) this.btnImgSiguiente.getScene().getWindow();
+        experiencia = (ExperienciaEntity) stage.getUserData();
+        txtExperienceName.setText(experiencia.getNombre());
+        txtExperienceDescription.setText(experiencia.getDescripcion());
+    }
+
+
 }
