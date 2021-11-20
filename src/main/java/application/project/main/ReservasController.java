ce9e873b03afd55acc67fe3278b70f515efe5a3d
Ari 2.0 <afainerman@correo.um.edu.uy>
2021-11-20 12:13:55 -0300

    "cambios"

--- src/main/java/application/project/main/ReservasController.java
+++ src/main/java/application/project/main/ReservasController.java
@@ -3,14 +3,24 @@ package application.project.main;
 import application.entities.ent.ExperienciaEntity;
 import application.entities.ent.OperadorEntity;
 import application.entities.ent.ReservaEntity;
+import application.entities.ent.ReservaRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ReservasController{
@@ -38,6 +48,33 @@ public class ReservasController{
 
     @FXML
     private Button btnExperiencia;
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @FXML
+    void initialize(){
+        CFechaInicio.setCellValueFactory(new MapValueFactory<>("fechaini"));
+        tcExperiencia.setCellValueFactory(new MapValueFactory<>("nombre"));
+        tcCantPersonas.setCellValueFactory(new MapValueFactory<>("cantidad"));
+
+        reservas = reservaRepository.findByOperadorExperiencia(operador.getMail());
+
+        ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
+
+        for (ReservaEntity res : reservas){
+            Map<String, Object> item = new HashMap<>();
+            item.put("fechaini", res.getFechaInicio());
+            item.put("nombre", res.getNombreExperiencia());
+            item.put("cantidad", res.getCantidad());
+            items.add(item);
+        }
+        TablaReservas.getItems().addAll(items);
+
+        Label placeholder = new Label();
+        placeholder.setText("No hay reservas proximas");
+        tvReservas.setPlaceholder(placeholder);
+    }
 
     @FXML
     void gotoExperiencia(ActionEvent event) {
